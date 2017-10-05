4d206c44ae79225e9e993f28c2a8a1cc40559740
smartjinyu <smartjinyu@gmail.com>
2017-10-05 15:05:39 +0800

    finish RMSprop and Adam

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -65,6 +65,8 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
+    v = config['momentum']*v-config['learning_rate']*dw
+    next_w = w + v
     pass
     ###########################################################################
     #                             END OF YOUR CODE                            #
@@ -99,6 +101,9 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
+    cache = config['decay_rate'] * config['cache'] + (1-config['decay_rate'])*dx**2
+    next_x = x - config['learning_rate'] * dx / (np.sqrt(cache) + config['epsilon'])
+    config['cache'] = cache
     pass
     ###########################################################################
     #                             END OF YOUR CODE                            #
@@ -136,6 +141,14 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
+    config['t'] += 1
+    m = config['beta1'] * config['m'] + (1-config['beta1']) * dx
+    mt = m / (1 - config['beta1']**config['t'])
+    v = config['beta2'] * config['v'] + (1-config['beta2']) * (dx**2)
+    vt = v / (1 - config['beta2']**config['t'])
+    next_x = x - config['learning_rate'] * mt / (np.sqrt(vt) + config['epsilon'])
+    config['m'] = m
+    config['v'] = v
     pass
     ###########################################################################
     #                             END OF YOUR CODE                            #
